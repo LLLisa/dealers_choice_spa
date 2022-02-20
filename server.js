de8c66d285289332cddf60d9d4ffae4b64745436
Lisa Knox <lisaknox83@gmail.com>
2022-02-20 17:06:26 -0500

    phase 3 done

---
+++ server.js
@@ -0,0 +1,93 @@
+//db--------------------------
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost/dealers_choice_spa');
+const faker = require('faker/dist/faker');
+
+const Human = db.define('human', {
+  name: {
+    type: Sequelize.STRING,
+  },
+});
+
+const Company = db.define('company', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  product: {
+    type: Sequelize.STRING,
+  },
+});
+
+Human.belongsTo(Company);
+Company.hasMany(Human);
+
+const syncAndSeed = async () => {
+  try {
+    await db.sync({ force: true });
+    await createCompany(3);
+    await createHuman(3);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let numberOfEmployers;
+const createCompany = async (num) => {
+  try {
+    for (let i = 0; i < num; i++) {
+      await Company.create({
+        name: faker.company.companyName(),
+        product: faker.commerce.productName(),
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createHuman = async (num) => {
+  try {
+    for (let i = 0; i < num; i++) {
+      await Human.create({
+        name: faker.name.findName(),
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//express---------------------
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+const init = async () => {
+  try {
+    await syncAndSeed();
+    console.log('~~~~~~~~~~~~~~~synced~~~~~~~~~~~~~~~');
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`glistening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
+
+//routies--------------------------
+app.get('/', (req, res) => {
+  res.redirect('/api');
+});
+
+app.get('/api', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  } catch (error) {
+    next(error);
+  }
+});
