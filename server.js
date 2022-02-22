cdb7d9b63716230298fd54623b62df5e2b35460f
Lisa Knox <lisaknox83@gmail.com>
2022-02-21 19:02:28 -0500

    working on adding and deleting entries

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const Human = db.define('human', {
   name: {
     type: Sequelize.STRING,
   },
+  quitJob: {
+    type: Sequelize.VIRTUAL,
+    get: function () {
+      this.companyId = null;
+    },
+  },
 });
 
 const Company = db.define('company', {
@@ -19,7 +25,7 @@ const Company = db.define('company', {
 });
 
 Human.belongsTo(Company);
-Company.hasMany(Human);
+Company.hasMany(Human, { as: 'employees' });
 
 const syncAndSeed = async () => {
   try {
@@ -31,6 +37,7 @@ const syncAndSeed = async () => {
   }
 };
 
+//randomize data seeding-------------------
 let numberOfEmployers;
 const createCompany = async (num) => {
   try {
@@ -40,6 +47,7 @@ const createCompany = async (num) => {
         product: faker.commerce.productName(),
       });
     }
+    numberOfEmployers = await Company.count();
   } catch (error) {
     console.log(error);
   }
@@ -48,8 +56,10 @@ const createCompany = async (num) => {
 const createHuman = async (num) => {
   try {
     for (let i = 0; i < num; i++) {
+      const rand = Math.ceil(Math.random() * numberOfEmployers);
       await Human.create({
         name: faker.name.findName(),
+        companyId: rand,
       });
     }
   } catch (error) {
@@ -61,8 +71,10 @@ const createHuman = async (num) => {
 const express = require('express');
 const app = express();
 const path = require('path');
+// const bodyParser = require('body-parser');
 
 app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use(express.urlencoded());
 
 const init = async () => {
   try {
@@ -94,7 +106,9 @@ app.get('/api', (req, res, next) => {
 
 app.get('/api/humans', async (req, res, next) => {
   try {
-    const response = await Human.findAll();
+    const response = await Human.findAll({
+      include: [Company],
+    });
     res.send(response);
   } catch (error) {}
 });
@@ -106,5 +120,36 @@ app.delete('/api/humans/:id', async (req, res, next) => {
     res.sendStatus(204);
   } catch (error) {
     next(error);
+  }
+});
+
+app.post('/api/humans', async (req, res, next) => {
+  try {
+    console.log(req.body);
+    res.send`
+    <html>
+      ${req.body}
+    </html>
+    `;
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/companies', async (req, res, next) => {
+  try {
+    const response = await Company.findAll({});
+    res.send(response);
+  } catch (error) {}
+});
+
+app.get(`/api/companies/:id`, async (req, res, next) => {
+  try {
+    const companyToList = await Company.findByPk(req.params.id, {
+      include: [{ model: Human, as: 'employees' }],
+    });
+    res.send(companyToList);
+  } catch (error) {
+    console.log(error);
   }
 });
--- src/index.js
+++ src/index.js
@@ -1,37 +1,81 @@
-console.log('hello world will do');
+const humanList = document.querySelector('#human-list');
+const companyList = document.querySelector('#company-list');
+const companySelector = document.querySelector('#the-selector');
+// const submitButton = document.querySelector('button');
+// console.log(quitButton);
 
-const uL = document.querySelector('ul');
-
-const removeItem = async (ev) => {
-  console.log(ev.target);
-  if (ev.target.tagName === 'LI') {
-    try {
+//list of employees by company doesn't update after employee delete
+const handleItem = async (ev) => {
+  try {
+    if (ev.target.id === 'human-name') {
       const itemId = ev.target.getAttribute('data-id');
+      // console.log(itemId);
       await axios.delete(`/api/humans/${itemId}`);
-    } catch (error) {
-      console.log(error);
     }
+    if (ev.target.id === 'company-name') {
+      const companyId = ev.target.getAttribute('data-id');
+      const companyToList = await axios.get(`/api/companies/${companyId}`);
+      // console.log(companyToList);
+      companyList.innerHTML = `
+      <h2>${companyToList.data.name}</h2>
+      ${companyToList.data.employees
+        .map((x) => {
+          return `<li>${x.name}</li>`;
+        })
+        .join('')}
+      `;
+    }
+    init();
+  } catch (error) {
+    console.log(error);
   }
-  init();
 };
 
-uL.addEventListener('click', removeItem);
+const quitJob = (ev) => {
+  console.log(ev.target.name);
+};
+
+const newHuman = async (ev) => {
+  try {
+    ev.preventDefault();
+    await axios.post('/api/humans');
+    console.log('hi');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const init = async () => {
   try {
     const content = await axios.get('/api/humans');
-    console.log(content);
-
-    uL.innerHTML = `
+    const mainList = `
       ${content.data
         .map((x) => {
-          return `<li data-id="${x.id}">${x.name}</li>`;
+          return `<li>
+          <span id="human-name" data-id="${x.id}">${x.name}</span> of 
+          <span id="company-name" data-id="${x.company.id}">${x.company.name}</span>
+          <button id="quit-job" >Quit</button>
+          </li>`;
         })
         .join('')}
     `;
+    const companyList = await axios.get('/api/companies');
+    const selectorList = `
+    ${companyList.data.map((x) => {
+      return `
+      <option value="${x.id}">${x.name}</option>
+      `;
+    })}
+    `;
+    humanList.innerHTML = mainList;
+    companySelector.innerHTML = selectorList;
   } catch (error) {
     console.log(error); //client console
   }
 };
 
 init();
+const quitButton = document.querySelector('#quit-job');
+
+humanList.addEventListener('click', handleItem);
+quitButton.addEventListener('click', quitJob);
